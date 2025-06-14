d925b6edbe2ed49c211c81e141b392855953cae8
jayashree-22374 <jayashreejayashree2207@gmail.com>
2025-06-14 18:49:38 +0530

    This is my identifier program

---
+++ C2TC_corejavaprogram/src/day1/Identifiers.java
@@ -0,0 +1,23 @@
+package day1;
+
+public class Identifiers {
+	public static void main(String[]args) {
+		
+	   int $num= 23;
+	   System.out.println("This is value is:"+$num);
+	
+			
+			int $number = 20; 
+			System.out.println("value of the number variable is : "+ $number);
+		
+			String studentName="Aniket";
+			System.out.println("Student name is : "+ studentName);
+			
+			String catName="Jenni";
+			System.out.println("My cat name is:"+catName);
+		}
+
+	
+	}
+
+
