19df1844bd8777d192e312a0d847d4bf6387ccf5
jayashree-22374 <jayashreejayashree2207@gmail.com>
2025-06-14 21:58:17 +0530

    This program is while loop program.

---
+++ C2TC_corejavaprogram/src/Day2/whileLoop.java
@@ -0,0 +1,18 @@
+package Day2;
+
+public class whileLoop {
+
+	
+		public static void main(String[] args) 
+		{
+			int i=90;
+			while(i<=100)
+			{
+				System.out.println(i);
+				i++;
+			}
+		}
+	}
+
+
+
