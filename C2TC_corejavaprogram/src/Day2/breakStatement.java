f6a4fcc04abaf14eb8387b762d137cf1aff69877
jayashree-22374 <jayashreejayashree2207@gmail.com>
2025-06-15 12:55:28 +0530

    This is break Statement program.

---
+++ C2TC_corejavaprogram/src/Day2/breakStatement.java
@@ -0,0 +1,12 @@
+package Day2;
+
+public class breakStatement {
+	public static void main(String[]args) {
+		for(int x=0;x<=10;x++) {
+			if(x==8)
+				break;
+			System.out.println("X is:"+x);
+		}
+	}
+
+}
