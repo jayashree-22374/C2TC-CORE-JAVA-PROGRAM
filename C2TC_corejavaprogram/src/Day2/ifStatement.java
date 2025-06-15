249d3f1d06be061fcecee0288238b767ccc852eb
jayashree-22374 <jayashreejayashree2207@gmail.com>
2025-06-15 12:12:39 +0530

    This program contains my if statement program.

---
+++ C2TC_corejavaprogram/src/Day2/ifStatement.java
@@ -0,0 +1,13 @@
+package Day2;
+
+public class ifStatement {
+	public static void main(String[]args) {
+		
+		int x=10;
+		if(x!= 10)
+			System.out.println("The value of x is 10");
+			System.out.println("The value of x is not 10");
+		
+	}
+
+}
