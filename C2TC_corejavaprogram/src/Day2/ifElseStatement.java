b7a7df2f9fb211ca7c4ee5a3619c62c40ab541a3
jayashree-22374 <jayashreejayashree2207@gmail.com>
2025-06-15 12:27:57 +0530

    This program is about eligible for voting or not.

---
+++ C2TC_corejavaprogram/src/Day2/ifElseStatement.java
@@ -0,0 +1,18 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class ifElseStatement {
+	public static void main(String[]args) {
+		
+			int x= 15;
+			if(x>=18)
+				System.out.println("You are eligible for voting");
+			else
+				System.out.println("You are not eligible for voting");
+		}
+			
+			
+	}
+
+
