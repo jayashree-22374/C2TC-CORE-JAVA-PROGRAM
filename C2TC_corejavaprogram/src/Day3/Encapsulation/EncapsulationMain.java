6e7909f206dd96fe17d63a7bfc6b4434e0e689ac
jayashree-22374 <jayashreejayashree2207@gmail.com>
2025-06-21 13:26:24 +0530

    This program contains Encapsulation program

---
+++ C2TC_corejavaprogram/src/Day3/Encapsulation/EncapsulationMain.java
@@ -0,0 +1,16 @@
+package Day3.Encapsulation;
+
+public class EncapsulationMain {
+	public static void main(String[]args) {
+	
+	Encapsulation obj=new Encapsulation();
+	obj.setSerialNum(102);
+	obj.setName("Jash");
+	obj.setAge(24);
+	System.out.println(obj);
+	
+	
+	}
+	
+
+}
