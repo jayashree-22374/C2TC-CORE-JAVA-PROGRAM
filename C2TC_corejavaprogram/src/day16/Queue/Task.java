c6f75d32649f374cfa8573d3397f5250eb15f5d3
jayashree-22374 <jayashreejayashree2207@gmail.com>
2025-08-05 20:44:52 +0530

    This contains my java program

---
+++ C2TC_corejavaprogram/src/day16/Queue/Task.java
@@ -0,0 +1,26 @@
+package day16.Queue;
+
+public class Task implements Comparable<Task> {
+	 private String name;
+	    private int priority;
+
+	    public Task(String name, int priority) {
+	        this.name = name;
+	        this.priority = priority;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+
+	    public int getPriority() {
+	        return priority;
+	    }
+
+	    @Override
+	    public int compareTo(Task other) {
+	        // Higher priority tasks will have lower values
+	        return Integer.compare(other.getPriority(), this.priority);
+	    }
+
+}
